4906147e9e00062343293af9a508c80d8899221d
Jon Bennett <jbennett571@gmail.com>
2012-12-16 19:28:37 -0500

    Making blob detection more efficient. Have cropped img, but currently no blobs detected.

--- src/ImageProcessor.cpp
+++ src/ImageProcessor.cpp
@@ -12,77 +12,98 @@ ImageProcessor::ImageProcessor(const char * imgMaskPath, int mediaWidth, int med
     imgSize.height = mediaHeight;
 
     imgMask = imread(imgMaskPath, CV_LOAD_IMAGE_ANYCOLOR); // Read the file
+    if (imgMask.cols == 0) {
+        printf("Couldn't Read: %s\n", imgMaskPath);
+    }
 
-    bgImg = imread("/Users/j3bennet/king_st_bg.jpg", CV_LOAD_IMAGE_ANYCOLOR); // Read the file // TODO: WTF is this ?
+    bgImg = imread("/Users/j3bennet/king_st_bg_temp.jpg", CV_LOAD_IMAGE_ANYCOLOR); // Read the file // TODO: WTF is this ?
+    if (bgImg.cols == 0) {
+        printf("Couldn't Read BG IMG: %s\n", "PATH");
+    }
     if (displayFrame) {
         cvNamedWindow("display", CV_WINDOW_AUTOSIZE);
     }
 }
 
-int ImageProcessor::processFrame(Mat frame)
+int ImageProcessor::processFrame(Mat in_frame)
 {
     // Mask Image
     CvBlobs cvBlobs;
+    Mat masked;
 
+    // Crop image
+    Rect roi(265, 230, 375, 250);
+    Mat cropped = in_frame(roi);
+
+    // To gray scale image
+    cvtColor(cropped, cropped, CV_BGR2GRAY);
+
+    //cv::imshow("dstImg", cropped);
+
+#if 1
+    //cvtColor(frame, frame, CV_BGR2RGB); // TODO: fix innefficiency
     try {
-        Mat masked;
-
-        cvtColor(frame, frame, CV_BGR2RGB); // TODO: fix innefficiency
-        cv::add(frame, imgMask, masked);
-
-        // Background Subtraction
-        if (fgimg.empty()) {
-            fgimg.create(img.size(), img.type());
-        }
-
-        //update the model
-        bool update_bg_model = true; //(blobs.size() > 0 && frameCount > 300) ? false : true;
-        bg_model(masked, fgmask, update_bg_model ? -1 : 0);
-
-        fgimg = Scalar::all(0);
-        masked.copyTo(fgimg, fgmask);
-        //filtered.copyTo(fgimg, fgimg);
-
-        Mat bgimg;
-        bg_model.getBackgroundImage(bgimg);
-
-        cvtColor(fgimg, fgimg, CV_BGR2GRAY);
-        //threshold(fgimg, fgimg, 100, 255, 3);
-        //fgimg = GetThresholdedImage(fgimg);
-        IplImage filtered_img = fgimg;
-
-        IplImage *labelImg = cvCreateImage(imgSize, IPL_DEPTH_LABEL, 1);
-        IplImage *dstImg = cvCreateImage(imgSize, IPL_DEPTH_8U, 3);
-
-        unsigned int result = cvLabel(&filtered_img, labelImg, cvBlobs);
-
-        cvFilterByArea(cvBlobs, 100, 2000);
-
-        if (counter) {
-            vector<Blob> blobs;
-            for (CvBlobs::iterator it = cvBlobs.begin(); it != cvBlobs.end(); ++it) {
-                    CvBlob blob = *(it->second);
-                    Blob * b = new Blob(blob, frameCount++);
-                    blobs.push_back(*b);
-            }
-        }
-
-        if (displayFrame) {
-            cvRenderBlobs(labelImg, cvBlobs, &filtered_img, dstImg);
-            cvShowImage("dstImg", dstImg);
-        }
-
-
-        cvReleaseImage(&labelImg);
-        cvReleaseImage(&dstImg);
-
-        if (!bgimg.empty()) {
-            //imshow("mean background image", bgimg );
-        }
-
-    } catch (cv::Exception& e) {
-        printf("Caught Exception in ImageProcessor: %s\n", e.what());
+    //cv::add(frame, imgMask, masked);
+    }     catch (cv::Exception& e) {
+        printf("Caught 1st Exception in ImageProcessor: %s\n", e.what());
     }
+
+try {
+    // Background Subtraction
+    if (fgimg.empty()) {
+        fgimg.create(cropped.size(), cropped.type());
+    }
+
+    //update the model
+    bool update_bg_model = true; //(blobs.size() > 0 && frameCount > 300) ? false : true;
+    bg_model(cropped, fgmask, update_bg_model ? -1 : 0);
+
+    fgimg = Scalar::all(0);
+    masked.copyTo(fgimg, fgmask);
+    //filtered.copyTo(fgimg, fgimg);
+} catch (cv::Exception& e) {
+    printf("Caught 2nd Exception in ImageProcessor: %s\n", e.what());
+}
+    //Mat bgimg;
+    //bg_model.getBackgroundImage(bgimg);
+
+    //cvtColor(fgimg, fgimg, CV_BGR2GRAY);
+    //threshold(fgimg, fgimg, 100, 255, 3);
+    //fgimg = GetThresholdedImage(fgimg);
+    IplImage filtered_img = fgimg;
+
+    IplImage *dstImg = cvCreateImage(imgSize, IPL_DEPTH_8U, 3);
+#endif
+    imgSize.width = 640;
+    imgSize.height = 480;
+    IplImage *labelImg = cvCreateImage(imgSize, IPL_DEPTH_LABEL, 1);
+    IplImage croppedIpl = in_frame;
+    unsigned int result = cvLabel(&filtered_img, labelImg, cvBlobs);
+    printf("RES %d\n", result);
+    cvShowImage("HMMM", &croppedIpl);
+    printf("BLOBS %d\n", cvBlobs.size());
+
+    cvFilterByArea(cvBlobs, 100, 2000);
+
+    if (counter) {
+        vector<Blob> blobs;
+        for (CvBlobs::iterator it = cvBlobs.begin(); it != cvBlobs.end(); ++it) {
+                CvBlob blob = *(it->second);
+                Blob * b = new Blob(blob, frameCount++);
+                blobs.push_back(*b);
+        }
+    }
+
+    if (displayFrame && false) {
+        IplImage frame = in_frame;
+        cvRenderBlobs(labelImg, cvBlobs, &frame, &frame);
+        ///cv::imshow("dstImg", cropped);
+    }
+
+
+    //cvReleaseImage(&labelImg);
+    //cvReleaseImage(&dstImg);
+
     return cvBlobs.size();
 }
 
